fa6c7916d994b101cd95482cddb36f25f2e5631b
Johan Alpne <johan.alpne@funnel.io>
2021-12-06 12:11:42 +0100

    day 6 solution

--- day6.py
+++ day6.py
@@ -1,24 +1,32 @@
-from helper.utils import solver, parse_file_rows_to_list
+from helper.utils import solver, parse_file_rows_to_list, list_str_to_list_number
+from collections import Counter
 
 
 DAY = 6
 
 
 def prepare_data():
-    raw_data = parse_file_rows_to_list(DAY, output_type=str)
-    return raw_data
+    raw_data = parse_file_rows_to_list(DAY, split_row_on=",")
+    return list_str_to_list_number(raw_data[0])
 
 
 @solver
 def part_a(data):
-    answer = data
-    return answer
+    return simulate_fish(data)
 
 
 @solver
 def part_b(data):
-    answer = data
-    return answer
+    return simulate_fish(data, days=256)
+
+
+def simulate_fish(all_fish, days=80):
+    ages = Counter(all_fish)
+    for day in range(days):
+        new_births = ages[day % 7]
+        ages[(day + 8) % 7] += new_births
+
+    return sum(ages.values())
 
 
 def main():
