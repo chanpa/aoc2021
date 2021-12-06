f8fc90a5894ff457bfb3ba6dc33f5cdf56a15161
Johan Alpne <johan.alpne@funnel.io>
2021-12-06 17:00:02 +0100

    FIXED IT

--- day6.py
+++ day6.py
@@ -1,5 +1,5 @@
 from helper.utils import solver, parse_file_rows_to_list, list_str_to_list_number
-from collections import Counter
+from collections import defaultdict
 
 
 DAY = 6
@@ -20,12 +20,21 @@ def part_b(data):
     return simulate_fish(data, days=256)
 
 
-def simulate_fish(all_fish, days=80):
-    ages = Counter(all_fish)
+def simulate_fish(all_fish_ages, days=80):
+    ages = defaultdict(lambda: [0, 0])
+    for fish in all_fish_ages:
+        ages[fish] = [ages[fish][0] + 1, 0]
+
     for day in range(days):
-        new_births = ages[day % 7]
-        ages[(day + 8) % 7] += new_births
-    return sum(ages.values())
+        birthday_group = day % 7
+        new_fish_delivery_day = (day + 9) % 7
+
+        adult_fish = ages[birthday_group][0]
+        ages[birthday_group][0] = adult_fish + ages[birthday_group][1]
+        ages[birthday_group][1] = 0
+
+        ages[new_fish_delivery_day][1] += adult_fish
+    return sum([group[0] + group[1] for group in ages.values()])
 
 
 def main():
