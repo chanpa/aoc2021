4decc82ced6d73779a54e569919e67b84addf86e
Johan Alpne <johan.alpne@gmail.com>
2021-12-08 09:09:50 +0100

    pushing because my desktop has biffed the project somehow

--- day8.py
+++ day8.py
@@ -4,14 +4,19 @@ from helper.utils import time_function, parse_file_rows_to_list
 DAY = 8
 
 
+@time_function
 def prepare_data():
-    raw_data = parse_file_rows_to_list(DAY, output_type=str)
+    raw_data = parse_file_rows_to_list(DAY, test=True, split_line=" | ")
     return raw_data
 
 
 @time_function
 def part_a(data):
-    answer = data
+    answer = 0
+    for _, output_values in data:
+        for digit in output_values:
+            if len(digit) in [2, 3, 4, 7]:
+                answer += 1
     return answer
 
 
