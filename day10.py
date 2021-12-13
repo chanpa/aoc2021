8145907ff4c01ab085c6d2c756b420a8e92d4c7f
Johan Alpne <johan.alpne@gmail.com>
2021-12-13 20:44:21 +0100

    day 10 solution

--- day10.py
+++ day10.py
@@ -1,39 +1,79 @@
-from collections import deque
+from collections import deque, Counter, defaultdict
 
 from helper.utils import time_function, parse_file_rows_to_list
 
 
 DAY = 10
+OPENERS = set("([{<")
+CLOSERS = set(")]}>")
 
 
 def prepare_data():
-    raw_data = parse_file_rows_to_list(DAY, test=True)
+    raw_data = parse_file_rows_to_list(DAY)
     return raw_data
 
 
 @time_function
 def part_a(data):
-    answer = data
-    return answer
+    return _calc_points_a(Counter([_char_stack(row) for row in data]))
 
 
 @time_function
 def part_b(data):
-    answer = data
-    return answer
+    chars = set(_char_stack(row) for row in data) - CLOSERS
+    return _calc_answer_b(chars)
 
 
-OPENERS = set("([{<")
-CLOSERS = set(")]}>")
-
-
-def _char_queue(chars):
+def _char_stack(chars):
     stack = deque()
     for ch in chars:
         if ch in OPENERS:
             stack.append(ch)
         else:
+            prev = stack.pop()
+            match abs(ord(ch) - ord(prev)) > 2:
+                case False:
+                    continue
+                case True:
+                    return ch
+    return _finish_incomplete_line(stack)
 
+
+def _finish_incomplete_line(stack):
+    completion_string = ""
+    for ch in reversed(stack):
+        if ch == "(":
+            completion_string += chr(ord(ch) + 1)
+        else:
+            completion_string += chr(ord(ch) + 2)
+    return completion_string
+
+
+def _calc_points_a(counter):
+    points = {
+        ")": 3,
+        "]": 57,
+        "}": 1197,
+        ">": 25137
+    }
+    return sum(counter[sign] * value for sign, value in points.items())
+
+
+def _calc_answer_b(lines):
+    points = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4
+    }
+    scores = []
+    for line in lines:
+        score = 0
+        for ch in line:
+            score *= 5
+            score += points[ch]
+        scores.append(score)
+    return sorted(scores)[len(scores) // 2]
 
 
 def main():
